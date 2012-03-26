c1b5e20f44261dfa03ababc70e0e24493847b37c
maggiewachs <maggie@filamentgroup.com>
2012-03-26 10:50:11 -0400

    updated attribution

--- js/rwd-nav.js
+++ js/rwd-nav.js
@@ -1,7 +1,7 @@
-/* Scripts for the tables test page 
-   Author: Maggie Wachs, www.filamentgroup.com
-   Date: November 2011
-   Dependencies: jQuery, jQuery UI widget factory
+/* Sample scripts for RWD nav patterns 
+   (c) 2012 Maggie Wachs, Filament Group, Inc - http://filamentgroup.com/examples/rwd-nav-patterns/GPL-LICENSE.txt
+   Last updated: March 2012
+   Dependencies: jQuery
 */
 
    
